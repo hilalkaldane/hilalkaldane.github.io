5a9785123dca5a0a39a7c65aef83024454eea646
kaldane <hilal.kaldane@here.com>
2025-12-07 02:22:52 +0530

    adds ga

--- src/index.js
+++ src/index.js
@@ -3,7 +3,10 @@ import { createRoot } from "react-dom/client";
 import { HashRouter } from "react-router-dom";
 import App from "./App";
 import "./index.css";
+import ReactGA from 'react-ga4'
 
+
+ReactGA.initialize('G-T6CZTM2KPY')
 createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <HashRouter>
